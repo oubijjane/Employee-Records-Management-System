3bd468e6f2fbbe3b5270ae824f80be6bf089681e
oubijjane <79411385+oubijjane@users.noreply.github.com>
2025-01-15 09:10:58 +0100

    add spring boot files

---
+++ src/main/java/com/records/demo/EmployeeRecordsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.records.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRecordsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRecordsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/records/demo/EmployeeRecordsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.records.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRecordsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
